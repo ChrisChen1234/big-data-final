9c4dd01d68c506e5b728f3efca01c9a93434a3ec
ChrisChen1234 <42106772+ChrisChen1234@users.noreply.github.com>
2018-09-07 18:09:38 +0800

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -23,7 +23,7 @@ import pandas as pd
 
 X_train = np.zeros((30, 7500, 4))
 
-for x in range(0,29):
+for x in range(0,39):
     X_train[x] = pd.read_excel('{num}.xls'.format(num = x)).drop('Col5',axis=1).values
 X_train = X_train*10**6
 
@@ -38,8 +38,8 @@ X_train = X_train*10**6
 
 X_test = np.zeros((10,7500,4))
 
-for x in range(30,39):
-    X_test[x-30] = pd.read_excel('{num}.xls'.format(num = x)).drop('Col5',axis=1).values
+for x in range(40,49):
+    X_test[x-40] = pd.read_excel('{num}.xls'.format(num = x)).drop('Col5',axis=1).values
 X_test = X_test*10**6
 
 
@@ -55,8 +55,8 @@ Ydata = np.array([0.490, 0.306, 0.418, 0.504, 0.499, 0.848, 0.654, 0.473, 0.453,
 # In[9]:
 
 
-Y_train = Ydata[0:30]
-Y_test = Ydata[30:40]
+Y_train = Ydata[0:40]
+#Y_test = Ydata[30:40]
 
 
 # In[12]:
@@ -88,10 +88,10 @@ model.fit(X_train, Y_train, epochs=600, batch_size=5)
 
 test_data = model.predict(X_test, batch_size=5)
 print ("test_data = \n",test_data,'\n\n')
-diff1 = abs(test_data-Y_test)
+#diff1 = abs(test_data-Y_test)
 #print ("test_Deviation = \n",diff1,'\n')
-diff1_sqr = diff1**2
-Test_RMSE =( diff1_sqr.sum()/10 )**0.5
+#diff1_sqr = diff1**2
+#Test_RMSE =( diff1_sqr.sum()/10 )**0.5
 #print("Test_RMSE = \n",Test_RMSE)
 
 
@@ -100,12 +100,12 @@ Test_RMSE =( diff1_sqr.sum()/10 )**0.5
 
 train_data = model.predict(X_train, batch_size=5)
 #print ("train_data = \n",train_data,'\n\n')
-diff2 = abs(train_data-Y_train)
-diff2_sqr = diff2**2
-Train_RMSE =( diff2_sqr.sum()/30 )**0.5
-Train_RMSE_test = (abs(train_data-Y_train)**2).sum()**0.5
-print("Train_RMSE = ",Train_RMSE)
-print("Train_RMSE_test = ", Train_RMSE_test)
+#diff2 = abs(train_data-Y_train)
+#diff2_sqr = diff2**2
+#Train_RMSE =( diff2_sqr.sum()/30 )**0.5
+#Train_RMSE_test = (abs(train_data-Y_train)**2).sum()**0.5
+#print("Train_RMSE = ",Train_RMSE)
+#print("Train_RMSE_test = ", Train_RMSE_test)
 
 
 
